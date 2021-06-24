712dd05ba76e436139012fb03a26b451568ca211
LHBank <wmbanksph@gmail.com>
2021-06-24 21:43:48 +0000

    Added home url to project level urls, and corresponding view to home/views.py. Content added to index.html in home app.

--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """ Index View """
+    return render(request, 'home/index.html')
+
+
+def about(request):
+    """ About Page View """
+    return render(request, 'home/about.html')
