f86e4635ae43cdcc19ff0e7c4260d99a77b5a20e
LHBank <wmbanksph@gmail.com>
2021-06-25 01:39:07 +0000

    Minor CSS additions to products.html. Added ordering to admin.py. Minor amendments for search queries in views.py.

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'gin_desc',
+        'alc_level',
+        'alc_vol',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'normal_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
