82cbb15aec8c166308717e431d26d8ce855fc3b6
LHBank <wmbanksph@gmail.com>
2021-06-27 15:43:12 +0000

    added functionality for admin users to control product management. Amended base/mob-top-head links to allow for navigation. Added info.html to home app with content. Tested all - ok.

--- products/views.py
+++ products/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import (
     render, redirect, reverse, get_object_or_404)
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 
 from .models import Product, Category
+from .forms import ProductForm
 
 
 def all_products(request):
@@ -39,9 +41,8 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(
-                    request, (
-                        "You'll need to enter something to search first."))
+                messages.error(request, (
+                    "You'll need to enter something to search first."))
                 return redirect(reverse('products'))
 
             queries = Q(
@@ -70,3 +71,71 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+@login_required
+def add_product(request):
+    """ Product Add View """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, Staff Only.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, 'Woohoo! More Gin to choose from!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Gin not updated. Try the form again.')
+    else:
+        form = ProductForm()
+
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def edit_product(request, product_id):
+    """ Product Edit View """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, Staff Only.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Great! All up to date!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Gin not updated. Try the form again.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_product(request, product_id):
+    """ Product Deletion """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, Staff Only.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
